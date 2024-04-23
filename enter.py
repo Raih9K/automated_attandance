f0f14f99882a10981e0a6dc03709d488da7622c1
Raihan Khan <isteyaqkhanraihan@gmail.com>
2024-04-23 23:10:07 +0600

    Initial commit

---
+++ enter.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://office.easyhrm.online/index.php")
+driver.maximize_window()
+
+try:
+    driver.find_element(
+        By.XPATH, "//input[@name='mobile']").send_keys("01961930719")
+    driver.find_element(
+        By.XPATH, "//input[@name='password']").send_keys("1")
+    driver.find_element(
+        By.XPATH, "//div[1]/form/div[3]/select/option[2]").click()
+    driver.find_element(
+        By.XPATH, "//input[@name='remarks']").send_keys("")
+    driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
+    time.sleep(10)
+    driver.save_screenshot('./screenshots/enter.png')
+    print("Print : Captured Screenshot")
+    print("Success: Script executed without errors.")
+except:
+    print("Failed")
+
+driver.quit()
